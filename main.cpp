eee3665caa3b026a93a4e4ab656c16f31c7eeaba
radx64 <radekteam@gmail.com>
2019-04-03 19:54:28 +0200

    Demo render

--- main.cpp
+++ main.cpp
@@ -2,9 +2,37 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Battle tanks DEMO!");
+    // Limit the framerate to 60 frames per second (this step is optional)
+    window.setFramerateLimit(60);
+
+
+    sf::Texture texture_body;
+    if (!texture_body.loadFromFile("../sprites/tankBody_blue_outline.png"))
+    {
+        return -1;
+    }
+
+    sf::Texture texture_cannon;
+    if (!texture_cannon.loadFromFile("../sprites/tankBlue_barrel1_outline.png"))
+    {
+        return -1;
+    }
+
+
+    sf::Sprite sprite_body;
+    sprite_body.setTexture(texture_body);
+    sprite_body.setPosition(sf::Vector2f(150.f, 150.f));
+    sf::Vector2u texture_body_size = texture_body.getSize();
+    sf::Vector2f texture_body_middle_point(texture_body_size.x / 2.f, texture_body_size.y / 2.f);
+    sprite_body.setOrigin(texture_body_middle_point);
+
+    sf::Sprite sprite_cannon;
+    sprite_cannon.setTexture(texture_cannon);
+    sprite_cannon.setPosition(sf::Vector2f(150.f, 150.f));
+    sf::Vector2u texture_cannon_size = texture_cannon.getSize();
+    sf::Vector2f texture_cannon_middle_point(texture_cannon_size.x / 2.f, texture_cannon_size.y / 2.f + 10.f);
+    sprite_cannon.setOrigin(texture_cannon_middle_point);
 
     while (window.isOpen())
     {
@@ -16,7 +44,10 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+	sprite_body.rotate(1.f);
+	sprite_cannon.rotate(-1.f);
+        window.draw(sprite_body);
+        window.draw(sprite_cannon);
         window.display();
     }
 
