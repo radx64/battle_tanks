5a7323f4f4f5fefb267a0aa8d56116433cd6f86a
Radek Szewczyk <radekteam@gmail.com>
2023-12-24 11:58:27 +0100

    Test files are only compiled on test targets. Added coverage support.

--- compile.sh
+++ compile.sh
@@ -2,5 +2,4 @@
 mkdir build
 cd build
 cmake -GNinja  ..
-#make
 ninja
---
+++ coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir build
+cd build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON ..
+ninja -v battle_tanks_testing_gui_coverage
+ninja -v battle_tanks_testing_math_coverage
+#ninja -v
