0e84ae6d35b771903fd6141e7c9dda1b6e1aad34
Radek Szewczyk <radekteam@gmail.com>
2024-01-20 11:44:00 +0100

    Added some road textures

--- source/graphics/TextureLibrary.cpp
+++ source/graphics/TextureLibrary.cpp
@@ -22,12 +22,6 @@ void TextureLibrary::initialize()
     initTexture("green_cannon",     "../sprites/tankGreen_barrel1_outline.png");
     initTexture("sand_cannon",      "../sprites/tankSand_barrel1_outline.png");
     initTexture("dark_cannon",      "../sprites/tankDark_barrel1_outline.png");
-    initTexture("grass_1",          "../sprites/tileGrass1.png");
-    initTexture("grass_2",          "../sprites/tileGrass2.png");
-    initTexture("sand_1",           "../sprites/tileSand1.png");
-    initTexture("sand_2",           "../sprites/tileSand1.png");
-    initTexture("grass_sand_NS",    "../sprites/tileGrass_transitionS.png");
-    initTexture("grass_sand_SN",    "../sprites/tileGrass_transitionN.png");
     initTexture("barrel_red",       "../sprites/barrelRed_top.png");
     initTexture("barrel_rust",      "../sprites/barrelRust_top.png");
     initTexture("barrel_green",     "../sprites/barrelGreen_top.png");
@@ -39,11 +33,48 @@ void TextureLibrary::initialize()
     initTexture("tree_brown_small", "../sprites/treeBrown_small.png");
     initTexture("tree_brown_large", "../sprites/treeBrown_large.png");
     initTexture("bullet_dark",      "../sprites/bulletDark1_outline.png");
-    initTexture("explosion_smoke_1","../sprites/explosionSmoke1.png");
-    initTexture("explosion_smoke_2","../sprites/explosionSmoke2.png");
-    initTexture("explosion_smoke_3","../sprites/explosionSmoke3.png");
-    initTexture("explosion_smoke_4","../sprites/explosionSmoke4.png");
-    initTexture("explosion_smoke_5","../sprites/explosionSmoke5.png");
+
+    //      PARTICLES       //
+    initTexture("explosion_smoke_1",    "../sprites/explosionSmoke1.png");
+    initTexture("explosion_smoke_2",    "../sprites/explosionSmoke2.png");
+    initTexture("explosion_smoke_3",    "../sprites/explosionSmoke3.png");
+    initTexture("explosion_smoke_4",    "../sprites/explosionSmoke4.png");
+    initTexture("explosion_smoke_5",    "../sprites/explosionSmoke5.png");
+
+    //      ROAD TEXTURES   //
+    initTexture("grass_1",              "../sprites/tileGrass1.png");
+    initTexture("grass_2",              "../sprites/tileGrass2.png");
+    initTexture("sand_1",               "../sprites/tileSand1.png");
+    initTexture("sand_2",               "../sprites/tileSand1.png");
+    initTexture("grass_sand_NS",        "../sprites/tileGrass_transitionS.png");
+    initTexture("grass_sand_SN",        "../sprites/tileGrass_transitionN.png");
+    initTexture("grass_road_ll",        "../sprites/tileGrass_roadCornerLL.png");
+    initTexture("grass_road_lr",        "../sprites/tileGrass_roadCornerLR.png");
+    initTexture("grass_road_ul",        "../sprites/tileGrass_roadCornerUL.png");
+    initTexture("grass_road_ur",        "../sprites/tileGrass_roadCornerUR.png");
+    initTexture("grass_road_cross",     "../sprites/tileGrass_roadCrossing.png");
+    initTexture("grass_road_ew",        "../sprites/tileGrass_roadEast.png");
+    initTexture("grass_road_ns",        "../sprites/tileGrass_roadNorth.png");
+    initTexture("grass_road_split_l",   "../sprites/tileGrass_roadSplitW.png");
+    initTexture("grass_road_split_r",   "../sprites/tileGrass_roadSplitE.png");
+    initTexture("grass_road_split_u",   "../sprites/tileGrass_roadSplitN.png");
+    initTexture("grass_road_split_d",   "../sprites/tileGrass_roadSplitS.png");
+    initTexture("sand_road_ll",         "../sprites/tileSand_roadCornerLL.png");
+    initTexture("sand_road_lr",         "../sprites/tileSand_roadCornerLR.png");
+    initTexture("sand_road_ul",         "../sprites/tileSand_roadCornerUL.png");
+    initTexture("sand_road_ur",         "../sprites/tileSand_roadCornerUR.png");
+    initTexture("sand_road_cross",      "../sprites/tileSand_roadCrossing.png");
+    initTexture("sand_road_ew",         "../sprites/tileSand_roadEast.png");
+    initTexture("sand_road_ns",         "../sprites/tileSand_roadNorth.png");
+    initTexture("sand_road_split_l",    "../sprites/tileSand_roadSplitW.png");
+    initTexture("sand_road_split_r",    "../sprites/tileSand_roadSplitE.png");
+    initTexture("sand_road_split_u",    "../sprites/tileSand_roadSplitN.png");
+    initTexture("sand_road_split_d",    "../sprites/tileSand_roadSplitS.png");
+    initTexture("grass_road_tranisiton_ns", "../sprites/tileGrass_roadTransitionS_dirt.png");
+    initTexture("grass_road_tranisiton_sn", "../sprites/tileGrass_roadTransitionN_dirt.png");
+    initTexture("grass_road_tranisiton_ew", "../sprites/tileGrass_roadTransitionE_dirt.png");
+    initTexture("grass_road_tranisiton_we", "../sprites/tileGrass_roadTransitionW_dirt.png");
+
 }
 
 sf::Texture& TextureLibrary::get(const std::string& name)
--- source/graphics/Tilemap.hpp
+++ source/graphics/Tilemap.hpp
@@ -17,9 +17,35 @@ enum class TileType
 {
     GRASS1,
     GRASS2,
+    GRASS_ROAD_LL,
+    GRASS_ROAD_LR,
+    GRASS_ROAD_UL,
+    GRASS_ROAD_UR,
+    GRASS_ROAD_CROSS,
+    GRASS_ROAD_EW,
+    GRASS_ROAD_NS,
+    GRASS_ROAD_SPLIT_L,
+    GRASS_ROAD_SPLIT_R,
+    GRASS_ROAD_SPLIT_U,
+    GRASS_ROAD_SPLIT_D,
+    SAND_ROAD_LL,
+    SAND_ROAD_LR,
+    SAND_ROAD_UL,
+    SAND_ROAD_UR,
+    SAND_ROAD_CROSS,
+    SAND_ROAD_EW,
+    SAND_ROAD_NS,
+    SAND_ROAD_SPLIT_L,
+    SAND_ROAD_SPLIT_R,
+    SAND_ROAD_SPLIT_U,
+    SAND_ROAD_SPLIT_D,
     SAND1,
     SAND2,
-    GRASS_SAND_N
+    GRASS_SAND_NS,
+    GRASS_ROAD_TRANISITON_NS,
+    GRASS_ROAD_TRANISITON_SN,
+    GRASS_ROAD_TRANISITON_EW,
+    GRASS_ROAD_TRANISITON_WE
 };
 
 }  // namespace
@@ -35,7 +61,33 @@ std::string toTextureName(const TileType type)
         case TileType::GRASS2 : return "grass_2";
         case TileType::SAND1  : return "sand_1";
         case TileType::SAND2  : return "sand_2";
-        case TileType::GRASS_SAND_N : return "grass_sand_NS";
+        case TileType::GRASS_SAND_NS : return "grass_sand_NS";
+        case TileType::GRASS_ROAD_LL : return "grass_road_ll";
+        case TileType::GRASS_ROAD_LR : return "grass_road_lr";
+        case TileType::GRASS_ROAD_UL : return "grass_road_ul";
+        case TileType::GRASS_ROAD_UR : return "grass_road_ur";
+        case TileType::GRASS_ROAD_CROSS : return "grass_road_cross";
+        case TileType::GRASS_ROAD_EW : return "grass_road_ew";
+        case TileType::GRASS_ROAD_NS : return "grass_road_ns";
+        case TileType::GRASS_ROAD_SPLIT_L : return "grass_road_split_l";
+        case TileType::GRASS_ROAD_SPLIT_R : return "grass_road_split_r";
+        case TileType::GRASS_ROAD_SPLIT_U : return "grass_road_split_u";
+        case TileType::GRASS_ROAD_SPLIT_D : return "grass_road_split_d";
+        case TileType::SAND_ROAD_LL : return "sand_road_ll";
+        case TileType::SAND_ROAD_LR : return "sand_road_lr";
+        case TileType::SAND_ROAD_UL : return "sand_road_ul";
+        case TileType::SAND_ROAD_UR : return "sand_road_ur";
+        case TileType::SAND_ROAD_CROSS : return "sand_road_cross";
+        case TileType::SAND_ROAD_EW : return "sand_road_ew";
+        case TileType::SAND_ROAD_NS : return "sand_road_ns";
+        case TileType::SAND_ROAD_SPLIT_L : return "sand_road_split_l";
+        case TileType::SAND_ROAD_SPLIT_R : return "sand_road_split_r";
+        case TileType::SAND_ROAD_SPLIT_U : return "sand_road_split_u";
+        case TileType::SAND_ROAD_SPLIT_D : return "sand_road_split_d";
+        case TileType::GRASS_ROAD_TRANISITON_NS : return "grass_road_tranisiton_ns";
+        case TileType::GRASS_ROAD_TRANISITON_SN : return "grass_road_tranisiton_sn";
+        case TileType::GRASS_ROAD_TRANISITON_EW : return "grass_road_tranisiton_ew";
+        case TileType::GRASS_ROAD_TRANISITON_WE : return "grass_road_tranisiton_we";
         default : return "unknown";
     }
 }
@@ -66,7 +118,36 @@ TileType idToType(const uint8_t id)
         case 2 : return TileType::GRASS2;
         case 3 : return TileType::SAND1; 
         case 4 : return TileType::SAND2; 
-        case 5 : return TileType::GRASS_SAND_N;
+        case 5 : return TileType::GRASS_SAND_NS;
+
+        case 11: return TileType::GRASS_ROAD_EW;
+        case 12: return TileType::GRASS_ROAD_NS;
+        case 13: return TileType::GRASS_ROAD_UL;
+        case 14: return TileType::GRASS_ROAD_UR;
+        case 15: return TileType::GRASS_ROAD_LR;
+        case 16: return TileType::GRASS_ROAD_LL;
+        case 20: return TileType::GRASS_ROAD_CROSS;
+        case 21: return TileType::GRASS_ROAD_SPLIT_U;
+        case 22: return TileType::GRASS_ROAD_SPLIT_R;
+        case 23: return TileType::GRASS_ROAD_SPLIT_D;
+        case 24: return TileType::GRASS_ROAD_SPLIT_L;
+
+        case 25: return TileType::GRASS_ROAD_TRANISITON_NS;
+        case 26: return TileType::GRASS_ROAD_TRANISITON_SN;
+        case 27: return TileType::GRASS_ROAD_TRANISITON_EW;
+        case 28: return TileType::GRASS_ROAD_TRANISITON_WE;
+
+        case 31: return TileType::SAND_ROAD_EW;
+        case 32: return TileType::SAND_ROAD_NS;
+        case 33: return TileType::SAND_ROAD_UL;
+        case 34: return TileType::SAND_ROAD_UR;
+        case 35: return TileType::SAND_ROAD_LR;
+        case 36: return TileType::SAND_ROAD_LL;
+        case 40: return TileType::SAND_ROAD_CROSS;
+        case 41: return TileType::SAND_ROAD_SPLIT_U;
+        case 42: return TileType::SAND_ROAD_SPLIT_R;
+        case 43: return TileType::SAND_ROAD_SPLIT_D;
+        case 44: return TileType::SAND_ROAD_SPLIT_L;
     }
     return TileType::GRASS1;
 }
@@ -96,26 +177,26 @@ public:
 private:
     uint8_t tileIds_[HEIGHT][WIDTH] = 
     {
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2},
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2},
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2},
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2},
-        {1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1},
-        {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5},
-        {4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4},
-        {3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3},
-        {4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4},
-        {3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3},
-        {4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4},
-        {3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3},
-        {4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4},
-        {3,4,3,4,3,4,3,4,3,4,3,4,3,4,3,3,4,3,4,3,4,3,4,3,4,3,4,3,4,3},
-        {4,3,4,3,4,3,4,3,4,3,4,3,4,3,4,4,3,4,3,4,3,4,3,4,3,4,3,4,3,4},
+        { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+        { 1,15,11,11,11,11,16, 2, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 2,12, 2, 1, 2, 1,12, 1, 2, 1, 2, 1, 2,15,11,11,11,11,16, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+        { 1,12, 1, 2, 1, 2,12, 2, 1, 2, 1, 2, 1,12, 1, 1, 2, 1,12, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 1,12, 1, 2, 1, 2,12, 2, 1, 2, 1, 2, 1,12, 1, 1, 2, 1,12, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 2,14,11,23,11,11,21,11,11,11,11,11,11,20,11,11,11,11,13, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+        { 1, 2, 1,12, 1, 2, 1, 2, 1, 2, 1, 2, 1,12, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 2, 1, 2,12, 2, 1, 2, 1, 2, 1, 2, 1, 2,12, 2, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2},
+        { 1, 2, 1,12, 1, 2, 1, 2, 1, 2, 1, 2, 1,12, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1},
+        { 5, 5, 5,25, 5, 5, 5, 5, 5, 5, 5, 5, 5,25, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+        { 4, 3, 4,32, 4, 3, 4, 3, 4, 3, 4, 3, 4,32, 4, 4, 3, 4, 3, 4, 3, 4, 3, 4,35,31,31,31,36, 4},
+        { 3, 4, 3,34,31,31,31,31,31,31,31,31,31,44, 3, 3, 4, 3, 4, 3, 4, 3, 4, 3,32, 3, 4, 3,32, 3},
+        { 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4,32, 4, 4, 3, 4, 3, 4, 3, 4, 3, 4,32, 4, 3, 4,32, 4},
+        { 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3,32, 3, 3, 4, 3, 4, 3, 4, 3, 4, 3,32, 3, 4, 3,32, 3},
+        { 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4,34,31,31,31,31,31,31,31,31,31,31,44, 4, 3, 4,32, 4},
+        { 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 3, 4, 3, 4, 3, 4, 3, 4, 3,34,31,31,31,33, 3},
+        { 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4},
+        { 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3},
+        { 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4},
     };
     sf::RenderTexture tiles_;
 };
