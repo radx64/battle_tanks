b71f39a0fd9922e957465e2cc773773db22e1858
Radek Szewczyk <radekteam@gmail.com>
2025-03-14 20:15:28 +0100

    TextureLibrary separation of declarations from definitions.

--- source/engine/TextureLibrary.hpp
+++ source/engine/TextureLibrary.hpp
@@ -13,38 +13,12 @@ public:
 
     // This can be used to force the initialization of the library
     // when lazy initialization is not prefered
-    static void init()
-    {
-        (void) instance();
-    }
-
-    sf::Texture& get(const std::string& name)
-    {
-        auto texture = textures_.find(name);
-        if (texture == textures_.end())
-        {
-            throw std::runtime_error("No texture called " + name + " found in library");
-        }
-        return texture->second;
-    }
+    static void init();
+    sf::Texture& get(const std::string& name);
 
 protected:
-    TextureLibrary()
-    {
-        const auto& assets = AssetsList::getAssets();
-
-        for(const auto& asset : assets)
-        {
-            initTexture(asset.first, asset.second);
-        }
-    }
-
-    void initTexture(const std::string& name, const std::string& path)
-    {
-        auto placedElement = textures_.emplace(std::make_pair(name, sf::Texture()));
-        bool hasLoaded = placedElement.first->second.loadFromFile(path);
-        if(not hasLoaded) throw std::runtime_error("Couldn't load texture file...");
-    }
+    TextureLibrary();
+    void initTexture(const std::string& name, const std::string& path);
 
     std::map<std::string, sf::Texture> textures_;
 };
@@ -54,6 +28,40 @@ TextureLibrary<AssetsList>& TextureLibrary<AssetsList>::instance()
 {
     static TextureLibrary instance;
     return instance;
+}
+
+template <typename AssetsList>
+void TextureLibrary<AssetsList>::init()
+{
+    (void) instance();
+}
+
+template <typename AssetsList>
+sf::Texture& TextureLibrary<AssetsList>::get(const std::string& name)
+{
+    auto texture = textures_.find(name);
+    if (texture == textures_.end())
+    {
+        throw std::runtime_error("No texture called " + name + " found in library");
+    }
+    return texture->second;
+}
+template <typename AssetsList>
+TextureLibrary<AssetsList>::TextureLibrary()
+{
+    const auto& assets = AssetsList::getAssets();
+
+    for(const auto& asset : assets)
+    {
+        initTexture(asset.first, asset.second);
+    }
+}
+template <typename AssetsList>
+void TextureLibrary<AssetsList>::initTexture(const std::string& name, const std::string& path)
+{
+    auto placedElement = textures_.emplace(std::make_pair(name, sf::Texture()));
+    bool hasLoaded = placedElement.first->second.loadFromFile(path);
+    if(not hasLoaded) throw std::runtime_error("Couldn't load texture file...");
 }
 
 }  // namespace engine
