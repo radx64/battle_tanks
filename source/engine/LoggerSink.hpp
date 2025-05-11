89b19d227c39cc90777d8d6dacd7792e6e588f87
Radek Szewczyk <radekteam@gmail.com>
2025-05-11 19:16:34 +0200

    Start experimenting with using FETCH_CONTENT for dependencies. Upgrade both SFML and libfmt to newer versions.

--- source/engine/LoggerSink.hpp
+++ source/engine/LoggerSink.hpp
@@ -13,7 +13,7 @@ namespace engine
     
 struct Log
 {
-    fmt::v9::color color;
+    fmt::v11::color color;
     std::string logType;
     std::string prefix;
     std::string text;
@@ -29,7 +29,7 @@ public:
     LoggerSink(const LoggerSink&) = delete;
     LoggerSink& operator=(const LoggerSink&) = delete;
     
-    void log(const fmt::v9::color& color, 
+    void log(const fmt::v11::color& color, 
         const std::string& logType,
         const std::string& prefix,
         const std::string& text);
@@ -39,7 +39,7 @@ public:
     ~LoggerSink();
     void stop();
     void processLogs();
-    void print(const fmt::v9::color color, 
+    void print(const fmt::v11::color color, 
         const std::string_view logType,
         const std::string_view prefix,
         const std::string_view log);
