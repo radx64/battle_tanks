cd79e1eafbbd35b30e8ade2dea9849f68ffa3a62
Radek Szewczyk <radekteam@gmail.com>
2026-02-22 20:40:50 +0100

    Update fmt library to version 12.1.0.

--- source/engine/LoggerSink.cpp
+++ source/engine/LoggerSink.cpp
@@ -47,7 +47,7 @@ LoggerSink::~LoggerSink()
     stop();
 }
 
-void LoggerSink::log(const fmt::v11::color& color, 
+void LoggerSink::log(const fmt::v12::color& color, 
     const std::string& logType,
     const std::string& prefix,
     const std::string& text)
@@ -78,7 +78,7 @@ void LoggerSink::stop()
     }
 }
 
-void LoggerSink::print(const fmt::v11::color color, 
+void LoggerSink::print(const fmt::v12::color color, 
     const std::string_view logType, 
     const std::string_view prefix,
     const std::string_view log)
