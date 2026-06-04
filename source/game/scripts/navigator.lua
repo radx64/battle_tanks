41e79f6984ea0cd3843c8fd27da5a605249f1341
Radek Szewczyk <radekteam@gmail.com>
2026-06-04 20:17:13 +0200

    [LUA] Fix navigation.lua to 'say' correct waypoint index

--- source/game/scripts/navigator.lua
+++ source/game/scripts/navigator.lua
@@ -24,12 +24,12 @@ function main()
                 end
 
                 -- Arrived (either was already within threshold or move_to returned)
+                say("Waypoint " .. current_waypoint_idx .. " reached");
                 current_waypoint_idx = current_waypoint_idx + 1;
                 if current_waypoint_idx > #waypoints then
                     current_waypoint_idx = 1;
                 end
                 set_throttle(0.0);
-                say("Waypoint " .. current_waypoint_idx .. " reached");
                 fire_cannon();
                 fire_cannon(); -- checking if cannon cooldown works, should only fire once
                 sleep(0.02);
