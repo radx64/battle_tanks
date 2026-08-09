c97b7bc00dd662918e92c58ad83defc17d54e161
Radek Szewczyk <radekteam@gmail.com>
2026-08-09 16:37:14 +0200

    [ENGINE] Enables PCH

---
+++ source/engine/source/PrecompiledHeaders.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <fmt/format.h>
